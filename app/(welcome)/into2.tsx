7f198d4c642929b1b98ff905bed99790c6d71753
Zakaria Bouchatal <bouchatal960@gmail.com>
2025-03-21 14:33:17 +0000

    i fucked up so hard men 8 mfrithach i wanna suicie

---
+++ app/(welcome)/into2.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const into2 = () => {
+  return (
+    <View>
+      <Text>into2</Text>
+    </View>
+  )
+}
+
+export default into2
+
+const styles = StyleSheet.create({})
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
+      <Stack.Screen name="(welcome)" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
